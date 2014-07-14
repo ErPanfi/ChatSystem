f4f08cef7663a95ef9a5753effd88bdfb3e32c1d
ErPanfi <erpanfi@gmail.com>
2014-07-14 19:13:53 +0200

    Using std::string when useful

--- ChatSystem/Address.cpp
+++ ChatSystem/Address.cpp
@@ -14,20 +14,19 @@ void Address::init(t_octet o1, t_octet o2, t_octet o3, t_octet o4, t_port port)
 	m_address = (t_address)(o1 << 24 | o2 << 16 | o3 << 8 | o4);
 }
 
-void Address::getAddressStr(t_addressStr address) const
+std::string Address::getAddressStr() const
 {
-	int i = 0;
-	itoa(getO1(), address, 10);
-	for(; address[i]!='\0';++i);	//skip to null char
-	address[i++] = '.';
-	itoa(getO2(),&(address[i]),10);
-	for(; address[i]!='\0';++i);	//skip to null char
-	address[i++] = '.';
-	itoa(getO3(),&(address[i]),10);
-	for(; address[i]!='\0';++i);	//skip to null char
-	address[i++] = '.';
-	itoa(getO4(),&(address[i]),10);
-	for(; address[i]!='\0';++i);	//skip to null char
-	address[i++] = ':';
-	itoa(m_port,&(address[i]),10);
+	std::string ret;
+	ret += getO1();
+	ret += '.';
+	ret += getO2();
+	ret += '.';
+	ret += getO3();
+	ret += '.';
+	ret += getO4();
+	ret += ':';
+	ret += m_port;
+
+
+	return ret;
 }
