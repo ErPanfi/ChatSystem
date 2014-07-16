1ff6c895598f4e53137dd755aa98a5d2565942ba
ErPanfi <erpanfi@gmail.com>
2014-07-16 12:46:53 +0200

    Added sending time to message and strict weak ordering between messages, so that we can use a std:: ordered data structure to memorize them

--- ChatSystem/Message.cpp
+++ ChatSystem/Message.cpp
@@ -8,25 +8,28 @@ int Message::pack(char buffer[]) const
 	*(bufferSentinel += CHAT_PROTO_NBYTES) = (char)t_dataType::MessageType;
 	++bufferSentinel;
 
-	for(int i = 0; i < MAX_MESSAGE_LEN && m_message[i] != '\0' && bufferSentinel - buffer < MAX_PACKET_SIZE; ++i, ++bufferSentinel)
+	*((PlatformUtils::t_relativeTime*)bufferSentinel) = m_sendingTime;		//TODO pack this with htond
+	bufferSentinel += sizeof(PlatformUtils::t_relativeTime)/sizeof(char);	
+
+
+	for(int i = 0; i < MAX_MESSAGE_LEN && m_message[i] != '\0' && bufferSentinel - buffer < MAX_PACKET_SIZE - 1; ++i, ++bufferSentinel)
 	{
 		*bufferSentinel = m_message[i];
 	}
 	*bufferSentinel = '\0';
 
-	//packet structure now is [PROTO_ID, PACKET_TYPE, Message_with_delimiter]
+	//packet structure now is [PROTO_ID, PACKET_TYPE, sending_time, Message_with_delimiter]
 
 	return bufferSentinel - buffer + 1;
 }
 
 void Message::unpack(char buffer[], int bufSize)
 {
-	int i, j;
-	//if we're at this point package has already been validated
-	for(j = 0, i = CHAT_PROTO_NBYTES + 1; i < bufSize && j < MAX_MESSAGE_LEN && buffer[i] != '\0'; ++i, ++j)
-	{
-		m_message[j] = buffer[i];
-	}
+	char* bufferSentinel = buffer + CHAT_PROTO_NBYTES + 1;	//skip first bytes, because already validated
 
-	m_message[j] = '\0';
+	m_sendingTime = *((PlatformUtils::t_relativeTime*)bufferSentinel);		//TODO unpack this with ntohd
+	bufferSentinel += sizeof(PlatformUtils::t_relativeTime)/sizeof(char);
+
+	m_message = std::string(bufferSentinel);
 }
+
--- ChatSystem/Message.h
+++ ChatSystem/Message.h
@@ -1,25 +1,58 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#include "PlatformUtils.h"
 #include "Packable.h"
+#include "User.h"
 #include <string>
 
 class Message : public Packable
 {
 public:
-	static const unsigned short MAX_MESSAGE_LEN = 141;
+	static const unsigned short MAX_MESSAGE_LEN = 140;
+
+	typedef std::string t_message;
 
 private:
-	char m_message[MAX_MESSAGE_LEN];
+	t_message m_message;
+	PlatformUtils::t_relativeTime m_sendingTime;
+	User *m_author;
 
 public:
 
-	std::string getMessage() const { return std::string(m_message);}
+	Message()
+		: m_sendingTime(PlatformUtils::time2relative())
+	{
+	}
+
+	inline PlatformUtils::t_relativeTime getSendingTime() const { return m_sendingTime; }
+	inline void setSendingTime(PlatformUtils::t_relativeTime newTime = PlatformUtils::time2relative()) { m_sendingTime = newTime; }	
+
+	inline t_message getMessage() const { return m_message;}
+	inline void setMessage(t_message newMessage) { m_message = newMessage; }
+
+	inline User* getAuthor() const { return m_author; }
+	inline void setAuthor(User* auth) { m_author = auth; }
 
 	virtual int pack(char buffer[]) const;
 	virtual void unpack(char buffer[], int bufSize);
 	virtual t_dataType getDataType() const { return t_dataType::MessageType;}
 
+	inline bool operator< (const Message &other) const		
+	{ 
+		return (
+					m_sendingTime > other.m_sendingTime ||	//the lesser message is the recentmost one
+					(
+						m_sendingTime == other.m_sendingTime && 
+						struct UserComparator()(m_author, other.m_author)
+					)
+				);
+	}
 };
 
-#endif
+struct MessageComparator
+{
+	inline bool operator()(const Message* const &lhs, const Message* const &rhs) { return lhs < rhs; }
+};
+
+#endif //MESSAGE_H
