c6c0c94841f966c86a43a1088eeb17989af4b9b7
ErPanfi <erpanfi@gmail.com>
2014-07-16 12:47:52 +0200

    Added reference for time frame

--- ChatSystem/PlatformUtils.cpp
+++ ChatSystem/PlatformUtils.cpp
@@ -6,6 +6,28 @@
 #endif
 
 #include <iostream>
+#include <time.h>
+
+time_t PlatformUtils::s_timeRef;
+
+void PlatformUtils::init()
+{
+	struct tm timeStamp;
+	memset(&timeStamp, 0, sizeof(timeStamp));
+	timeStamp.tm_year = 2014 - 1900;
+
+	s_timeRef = mktime(&timeStamp);	//time reference is 01/01/2014
+}
+
+PlatformUtils::t_relativeTime PlatformUtils::time2relative(time_t absTime)
+{
+	if(!absTime)
+	{
+		time(&absTime);
+	}
+
+	return (t_relativeTime) difftime(absTime, s_timeRef);
+}
 
 // returns 0 if no key pressed
 char PlatformUtils::currKeyPressed()
@@ -26,10 +48,21 @@ void PlatformUtils::waitForNextFrame(double seconds)
 #endif
 }
 
-
 void PlatformUtils::log(std::string message)
 {
 	std::cout << message << std::endl;
 }
 
+void PlatformUtils::printFooter()
+{
+	std::cout << "Type command key (\'?\' for command list) : ";
+}
 
+void PlatformUtils::printCommandList()
+{
+	std::cout	<< "'U' : print connected users list" << std::endl //
+				<< "'M' : write a new message (blocks packets reception until input completed)"	<< std::endl //
+				<< "\'R\' print last messages received" << std::endl //
+				<< "\'?\' : print this command list" << std::endl //
+				<< " \'Esc\' : Quit" << std::endl;
+}
--- ChatSystem/PlatformUtils.h
+++ ChatSystem/PlatformUtils.h
@@ -1,16 +1,26 @@
-#ifndef INPUT_MANAGER_H
-#define INPUT_MANAGER_H
+#ifndef PLATFORM_UTILS_H
+#define PLATFORM_UTILS_H
 
 #include "PlatformDetection.h"
 #include <string>
 
 class PlatformUtils
 {
+private:
+	static time_t s_timeRef;
+
 public:
 	static const char NO_CHAR_READ = '\0';
 	static char currKeyPressed();
 	static void waitForNextFrame(double seconds);
 	static void log(std::string message);
+
+	typedef unsigned long t_relativeTime;
+	static void init();
+	static t_relativeTime time2relative(time_t absTime = 0);
+
+	static void printFooter();
+	static void printCommandList();
 };
 
-#endif
+#endif	//PLATFORM_UTILS_H
